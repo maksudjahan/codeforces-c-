7d504b35e0a6f3eb8b9d45750c30708e16c87481
Maksud Jahan <maksudjahan7@gmail.com>
2026-06-28 18:39:47 +0600

    Add files via upload

---
+++ Calculating function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int long long n;
+    cin>>n;
+  if(n%2==0)
+    cout<<n/2<<endl;
+    if(n%2!=0)
+        cout<<-(n+1)/2<<endl;
+}
