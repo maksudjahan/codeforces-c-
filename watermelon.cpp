11183278d1cf6053d21066f3bed03fb68db358b0
Maksud Jahan <maksudjahan7@gmail.com>
2026-06-28 15:50:34 +0600

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int w;
+    cin>>w;
+    if((w>=6)&&(w<=100)&&(w%2==0))
+    cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+
+}
+    int w;
+    cin>>w;
+    if((w>=6)&&(w<=100)&&(w%2==0))
+    cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+
+}
