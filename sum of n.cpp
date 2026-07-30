3a3990f0b1924b4e625181d4418038db3253d4ea
Maksud Jahan <maksudjahan7@gmail.com>
2026-07-30 23:17:53 +0600

    Add files via upload

---
+++ sum of n.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;long long sum=0;
+    cin>>n;
+    if(n>0){
+    for(int i=0;i<=n;i++){
+      sum=sum+i;
+    }}
+
+    cout<<sum<<endl;
+    return 0;
+}
