ff25b461984aff14e80ee8a60c9aa47f75d74605
Maksud Jahan <maksudjahan7@gmail.com>
2026-07-30 23:29:15 +0600

    Add files via upload

---
+++ wrong_substraction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    for(int i=0;i<b;i++){
+        if(a%10==0)
+        a=a/10;
+        else
+            a=a-1;
+
+    }
+    cout<<a<<endl;
+}
